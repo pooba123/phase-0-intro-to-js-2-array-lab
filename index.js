4f033ea367116a8de280e29821fbf6d5f4bab6f3
yunis <123456789+empowerwithit@users.noreply.github.com>
2024-06-09 15:35:30 +0300

    assignment - arrays- done

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+let cats =["Milo", "Otis", "Garfield"]
+function  destructivelyAppendCat(name){
+ cats.push('Ralph')
+ 
+}
+
+function destructivelyPrependCat (name) {
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat(name) {
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(name) {
+    cats.shift()
+}
+function appendCat(name ="Broom"){
+    let catsOne =[...cats, name]
+    return catsOne
+    
+    
+
+}
+function prependCat(name){
+    let newArray = [name,...cats]
+    return newArray
+}
+
+function removeLastCat(){
+    let newArray = cats.slice (0,2)
+    return  newArray
+}
+function removeFirstCat(){
+    let newArray = cats.slice (1)
+    return  newArray
+}
